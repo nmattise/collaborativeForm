beb36ea8595991c473a000d9353d24b827852225
Nicholas Mattise <n.mattise@gmail.com>
2014-04-18 11:28:43 -0400

    added fibonacci sequence

--- routes/routes.js
+++ routes/routes.js
@@ -7,11 +7,30 @@ module.exports = {
             title: 'Express'
         });
     },
-    runFibo: function(request,response){
+    runFibo: function (request, response) {
         console.log(request.body);
         var userName = request.body.userName;
+        var fiboNum = request.body.fiboNumber;
+        var fibResults = [];
+        function fibo(maxNum){
+            var first, second, add;
+            for( i=0; i<maxNum;i++){
+                if(i === 0){
+                    first = 1;
+                    second = 2;
+                }
+                add = first + second;
+                first = second;
+                second = add;
+                fibResults.push(add);
+            }
+            console.log(add);
+        }
+        fibo(fiboNum);
         response.render('results', {
-            userName:userName
+            userName: userName,
+            results: fibResults,
+            fiboNum:fiboNum
         });
     }
 }
